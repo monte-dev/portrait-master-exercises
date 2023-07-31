13a9dd418aeb1298a032e1bedaef893cf12cd057
monte-dev <badziobw@gmail.com>
2023-08-01 00:42:57 +0100

    add security for photos form on server side

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -3,54 +3,69 @@ const Photo = require('../models/photo.model');
 /****** SUBMIT PHOTO ********/
 
 exports.add = async (req, res) => {
+	try {
+		const { title, author, email } = req.fields;
+		const file = req.files.file;
 
-  try {
-    const { title, author, email } = req.fields;
-    const file = req.files.file;
+		if (title && author && email && file) {
+			// if fields are not empty...
+			const regex = new RegExp(
+				/(<\s*(strong|em)*>(([A-z]|\s)*)<\s*\/\s*(strong|em)>)|(([A-z]|\s|\.)*)/,
+				'g'
+			);
+			const validTitle = title.match(regex).join('');
+			const validAuthor = author.match(regex).join('');
 
-    if(title && author && email && file) { // if fields are not empty...
+			const emailRegex = new RegExp('[a-z0-9]+@[a-z]+.[a-z]{2,3}');
+			const validEmail = email.match(emailRegex);
 
-      const fileName = file.path.split('/').slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
-      const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
-      await newPhoto.save(); // ...save new photo in DB
-      res.json(newPhoto);
-
-    } else {
-      throw new Error('Wrong input!');
-    }
-
-  } catch(err) {
-    res.status(500).json(err);
-  }
-
+			const fileName = file.path.split('.').slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
+			if (
+				(fileExt === 'jpg' || fileExt === 'png' || fileExt === 'gif') &&
+				validEmail &&
+				validTitle &&
+				validAuthor
+			) {
+				const newPhoto = new Photo({
+					title,
+					author,
+					email,
+					src: fileName,
+					votes: 0,
+				});
+				await newPhoto.save(); // ...save new photo in DB
+				res.json(newPhoto);
+			}
+		} else {
+			throw new Error('Wrong input!');
+		}
+	} catch (err) {
+		res.status(500).json(err);
+	}
 };
 
 /****** LOAD ALL PHOTOS ********/
 
 exports.loadAll = async (req, res) => {
-
-  try {
-    res.json(await Photo.find());
-  } catch(err) {
-    res.status(500).json(err);
-  }
-
+	try {
+		res.json(await Photo.find());
+	} catch (err) {
+		res.status(500).json(err);
+	}
 };
 
 /****** VOTE FOR PHOTO ********/
 
 exports.vote = async (req, res) => {
-
-  try {
-    const photoToUpdate = await Photo.findOne({ _id: req.params.id });
-    if(!photoToUpdate) res.status(404).json({ message: 'Not found' });
-    else {
-      photoToUpdate.votes++;
-      photoToUpdate.save();
-      res.send({ message: 'OK' });
-    }
-  } catch(err) {
-    res.status(500).json(err);
-  }
-
+	try {
+		const photoToUpdate = await Photo.findOne({ _id: req.params.id });
+		if (!photoToUpdate) res.status(404).json({ message: 'Not found' });
+		else {
+			photoToUpdate.votes++;
+			photoToUpdate.save();
+			res.send({ message: 'OK' });
+		}
+	} catch (err) {
+		res.status(500).json(err);
+	}
 };
