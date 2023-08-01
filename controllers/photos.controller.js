77ce67f3a3b07b4762d009949858edfecd3f43bb
monte-dev <badziobw@gmail.com>
2023-08-01 17:52:21 +0100

    add voter model and voter ip check on server side

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -1,4 +1,6 @@
 const Photo = require('../models/photo.model');
+const Voter = require('../models/Voter.model');
+const requestIp = require('request-ip');
 
 /****** SUBMIT PHOTO ********/
 
@@ -19,9 +21,13 @@ exports.add = async (req, res) => {
 			const emailRegex = new RegExp('[a-z0-9]+@[a-z]+.[a-z]{2,3}');
 			const validEmail = email.match(emailRegex);
 
-			const fileName = file.path.split('.').slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
+			const fileName = file.path.split('/').slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
+			const fileExtension = fileName.path.split('.').slice(-1)[0];
+
 			if (
-				(fileExt === 'jpg' || fileExt === 'png' || fileExt === 'gif') &&
+				(fileExtension === 'jpg' ||
+					fileExtension === 'png' ||
+					fileExtension === 'gif') &&
 				validEmail &&
 				validTitle &&
 				validAuthor
@@ -58,13 +64,30 @@ exports.loadAll = async (req, res) => {
 
 exports.vote = async (req, res) => {
 	try {
-		const photoToUpdate = await Photo.findOne({ _id: req.params.id });
-		if (!photoToUpdate) res.status(404).json({ message: 'Not found' });
-		else {
-			photoToUpdate.votes++;
-			photoToUpdate.save();
-			res.send({ message: 'OK' });
+		const clientIp = requestIp.getClientIp(req);
+		const voter = await Voter.findOne({ user: clientIp });
+		const photoToUpdate = await Photo.findById(req.params.id);
+
+		if (!voter) {
+			const newVoter = new Voter({
+				user: clientIp,
+				votes: [req.params.id],
+			});
+			await newVoter.save();
+		} else {
+			if (voter.votes.includes(req.params.id)) {
+				console.log('already voted');
+				return res.status(500).json({
+					message: 'You have already voted...',
+				});
+			} else {
+				voter.votes.push(req.params.id);
+				photoToUpdate.votes++;
+				await voter.save();
+			}
 		}
+		await photoToUpdate.save();
+		res.send({ message: 'OK' });
 	} catch (err) {
 		res.status(500).json(err);
 	}
--- db.js
+++ db.js
@@ -4,19 +4,21 @@ const mongoose = require('mongoose');
 const loadTestData = require('./testData');
 
 const connectToDB = () => {
+	// connect to DB
+	mongoose.connect('mongodb://localhost:27017/photosDB', {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	});
+	const db = mongoose.connection;
 
-  // connect to DB
-  mongoose.connect('mongodb://localhost:27017/photosDB', { useNewUrlParser: true, useUnifiedTopology: true });
-  const db = mongoose.connection;
+	// on success
+	db.once('open', () => {
+		console.log('Connected to the database');
+		loadTestData();
+	});
 
-  // on success
-  db.once('open', () => {
-    console.log('Connected to the database');
-    loadTestData();
-  });
-
-  // on error
-  db.on('error', (err) => console.log('Error ' + err));
-}
+	// on error
+	db.on('error', (err) => console.log('Error ' + err));
+};
 
 module.exports = connectToDB;
